0c92df295710194117700e32f0a7ffab2bf91d93
Ernesto Storani Vidaurre <125527244+nestto@users.noreply.github.com>
2023-11-09 16:22:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Variáveis 
+const nomeHeroi = "Ernestinho";
+let xpHeroi = 20000;
+
+// Estrutura de decisão if/else if/else
+
+let nivel= ""; //Aqui deixamos em braco para ser preenchido com os valores abaixo
+
+if (xpHeroi < 1000) {
+  nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+  nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+  nivel = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+  nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+  nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+  nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+  nivel = "Imortal";
+} else if (xpHeroi >= 10001){
+  nivel = "Radiante";
+} else {
+    nivel = "Não foi possível identificar o nível";
+}
+
+// Exibindo mensagem de
+console.log(`O Herói de nome: ${nomeHeroi} está no nível ${nivel}`);
